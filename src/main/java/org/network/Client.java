f578af98e7bb4f015f9b5df183132cdef6844510
s-Aura-v <slamich2@oswego.edu>
2025-03-16 14:58:55 -0400

    Reactor Setup
    1.         // TODO: See if I need to attach an acceptor
    2. check the run method again
    3. block or non blocking?

---
+++ src/main/java/org/network/Client.java
@@ -0,0 +1,9 @@
+package org.network;
+
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
+public class Client {
+    private static final String host = "localhost";
+
+}
---
+++ src/main/java/org/network/Reactor.java
@@ -0,0 +1,71 @@
+package org.network;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Reactor implements Runnable {
+    private static final int PORT = 26880;
+    final Selector selector;
+    final ServerSocketChannel serverSocket;
+
+    public Reactor(int port) throws IOException {
+        selector = Selector.open();
+        serverSocket = ServerSocketChannel.open();
+        serverSocket.socket().bind(new InetSocketAddress(port));
+
+        // NOTE: If you are dealing with multiple clients, it should be set to false.
+        // In this project, we only deal with one client, hence blocking is true.
+        serverSocket.configureBlocking(true);
+
+        // register what to do when I get a connection, whenever it happens, do it please
+        SelectionKey key = serverSocket.register(selector, SelectionKey.OP_ACCEPT);
+        // TODO: See if I need to attach an acceptor
+//        key.attach();
+    }
+
+
+    // Dispatch Loop
+    // here, you can use as many threads as you want.
+    // but for project 2, only one thread is needed because there’s only one communication.
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                selector.select();
+                Set<SelectionKey> selected = selector.selectedKeys();
+                Iterator<SelectionKey> it = selected.iterator();
+                while (it.hasNext()) {
+                    SelectionKey key = it.next();
+//                    it.remove(); (should i remove the key?)
+                    dispatch(key);
+                }
+                selected.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void dispatch(SelectionKey key) {
+        Runnable r = (Runnable) key.attachment();
+        if (r != null) {
+            r.run();
+        }
+    }
+    // End of Dispatch Loop
+
+    public static void main(String[] args) {
+
+    }
+}
