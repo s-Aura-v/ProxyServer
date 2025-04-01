8f1687223035acebb74f8d5ccd149eb47be15ada
s-Aura-v <slamich2@oswego.edu>
2025-04-01 13:38:25 -0400

    Throughput Calculation Added
    NanoTime() is a good indicator for time.
    Don't forget to multiply bytes by 8.

--- src/main/java/org/network/Client.java
+++ src/main/java/org/network/Client.java
@@ -33,7 +33,8 @@ public class Client implements Runnable {
                 System.out.println("Client: " + "Sending url " + urlNum);
                 urlNum++;
 
-                long startTime = System.currentTimeMillis();
+                long startTime = System.nanoTime();
+                System.out.println(startTime);
                 clientChannel.write(ByteBuffer.wrap(urlPacket));
                 ByteBuffer dataBuffer = ByteBuffer.allocate(MAX_PACKET_SIZE);
                 ArrayList<byte[]> packets = new ArrayList<>();
@@ -49,18 +50,18 @@ public class Client implements Runnable {
                     }
                     dataBuffer.flip();
 
-
                     byte[] data = new byte[dataBuffer.limit()];
                     dataBuffer.get(data);
 
                     int blockNumber = ((data[2] & 0xff) << 8) | (data[3] & 0xff);
                     byte[] ack = createACKPacket(blockNumber);
                     clientChannel.write(ByteBuffer.wrap(ack));
-                    long endTime = System.currentTimeMillis();
 
                     packets.add(data);
                     dataBuffer.clear();
                 }
+                long endTime = System.nanoTime();
+                System.out.println(endTime);
 
                 ByteArrayOutputStream output = new ByteArrayOutputStream();
                 for (byte[] imagePacket : packets) {
@@ -70,6 +71,14 @@ public class Client implements Runnable {
                 byte[] finalImageFrame = output.toByteArray();
                 bytesToImage(finalImageFrame, safeURL);
                 output.close();
+
+                // Measuring Throughput
+                double imageSizeInMB = (finalImageFrame.length * 8) / 1e6;
+                System.out.println("Image Size in MB: " + imageSizeInMB);
+                double packetsTimeInSeconds = (endTime - startTime) / 1e9;
+                System.out.println("Packet Time in Seconds: " + packetsTimeInSeconds);
+                double throughput = (imageSizeInMB/packetsTimeInSeconds);
+                System.out.println("The throughput of this operation was " + throughput + " Mb/s");
 
                 System.out.println("Please enter the image address or enter 'exit' to terminate program: ");
                 url = scanner.nextLine();
