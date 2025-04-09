b7ab5436ffe62c82092f781a436a031bd9e9161f
s-Aura-v <slamich2@oswego.edu>
2025-04-09 13:22:30 -0400

    Data Collection + HTML Addons

--- src/main/java/Sequential/Client.java
+++ src/main/java/Sequential/Client.java
@@ -103,7 +103,7 @@ public class Client {
             } catch (IOException e) {
                 System.out.println("Client " + urlNum + " failed to connect");
             } finally {
-                Files.write(Path.of("src/main/resources/data/local-moxie, 64, drops"), throughputData, Charset.defaultCharset());
+                Files.write(Path.of("src/main/resources/data/local-moxie, 1, drops"), throughputData, Charset.defaultCharset());
             }
         }
     }
